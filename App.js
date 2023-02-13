5ceea97bbf53c9f2cad54160efd79636387db078
vladvlas <vladvlas175@gmail.com>
2023-02-13 13:22:19 +0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import AnimatedLottieView from "lottie-react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <AnimatedLottieView duration={2000} source={require("./assets/133564-typing.json")} autoPlay={true} />
     </View>
   );
 }
